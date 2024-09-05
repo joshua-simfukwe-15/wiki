9e1efd677aeaf477de4d8a853dd22a7f899f6882
joshua Simfukwe <joshuasimfukwe101@gmail.com>
2024-09-05 15:35:50 +0200

    feat: add entry page view for displaying encyclopedia entries
    - Implement view to render content for entries at /wiki/TITLE
    - Fetch entry content using util.get_entry function
    - Display error page if requested entry does not exist
    - Set the page title to the name of the encyclopedia entry when it exists

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -2,9 +2,24 @@ from django.shortcuts import render
 
 from . import util
 
+from markdown2 import Markdown
+
+
 
 def index(request):
     return render(request, "encyclopedia/index.html", {
         "entries": util.list_entries()
+    })
+
+def entry(request, title):
+    entry = util.get_entry(title)
+    markdowner = Markdown()
+
+    if entry == None:
+        entry = f"## {title.capitalize()}\'s page has not been found"
+    entry=markdowner.convert(entry)    
+    return render(request, "encyclopedia/entry.html", {
+        "title":title,
+        "entry": entry
     })
 
