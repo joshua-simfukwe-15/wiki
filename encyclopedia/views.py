45fcc82471ea34e6d6f8d534aec26e0c23bdc83a
joshua Simfukwe <joshuasimfukwe101@gmail.com>
2024-09-05 17:33:59 +0200

    feat: add search functionality to encyclopedia
    - Enable users to search for encyclopedia entries using the sidebar search box
    - Redirect user to the entry page if the search query matches an entry title
    - Display a search results page if the query does not match an exact title
    - Show entries that contain the query as a substring in the search results
    - Allow users to click on search result entries to be taken to their respective pages

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
 from . import util
 
@@ -23,3 +23,22 @@ def entry(request, title):
         "entry": entry
     })
 
+def search(request):
+    query = request.GET.get('q')  # Get the search query from the request
+    if query:
+        entries = util.list_entries()
+        matching_entries = [entry for entry in entries if query.lower() in entry.lower()]
+
+        if len(matching_entries) == 1 and matching_entries[0].lower() == query.lower():
+            # If there's an exact match, redirect to that entry's page
+            return redirect('entry', title=matching_entries[0])
+
+        # Otherwise, show the search results
+        return render(request, "encyclopedia/search.html", {
+            "entries": matching_entries,
+            "query": query
+        })
+    else:
+        return render(request, "encyclopedia/index.html", {
+            "entries": util.list_entries()
+        })
